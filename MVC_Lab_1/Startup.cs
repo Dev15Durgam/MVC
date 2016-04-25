3cead72cf9587f24391464138bda855f30d59397
Dev15Durgam <blackros_91@hotmail.com>
2016-04-25 10:35:25 +0200

    Add project files.

---
+++ MVC_Lab_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Lab_1.Startup))]
+namespace MVC_Lab_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
