2c85c583c1fa9e4aad9fdb6d7f40913197a33196
Dev15Durgam <blackros_91@hotmail.com>
2016-05-25 14:54:01 +0200

    Lab_2_2 & Lab_4_2 klara

--- Lab_2_2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab_2_2.MvcApplication" Language="C#" %>
---
+++ Lab_4_1/Controllers/TwentyOneController.cs
@@ -0,0 +1,42 @@
+﻿using Lab_4_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_4_1.Controllers
+{
+    public class TwentyOneController : Controller
+    {
+        // GET: TwentyOne
+        [HttpGet]
+        public ActionResult TwentyOneGame()
+        {
+            var game = new _21GameModel();
+            return View(game);
+        }
+        [HttpPost]
+        public ActionResult TwentyOneGame(_21GameModel game)
+        {
+            game.UserInputLogic();
+
+            if (game.WinLogic() && game.ComputerWin == false)
+            {
+                ViewBag.result = "player win";
+                game.CurrentNumber = 0; //Reset after a finished game
+            }
+            game.ResponseLogic();
+
+            if (game.WinLogic() && game.ComputerWin == true)
+            {
+                ViewBag.result = "Computer Win";
+                game.CurrentNumber = 0; //Reset after a finished game
+            }
+
+            ModelState.Remove("CurrentNumber");
+            return View(game);
+
+        }
+    }
+}
